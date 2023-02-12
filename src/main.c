70de86d69b01e0771f3bd4290ac49e5827f7c19c
laurijssen <lauserve@hotmail.com>
2023-02-12 07:29:22 -0500

    initial commit with Makefile

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+  
+#define clear() printf("\033[H\033[J")
+  
+void init_shell()
+{
+    clear();
+    printf("\n\n******************"
+        "************************");
+    printf("\n\nDARK PIVOT");
+    printf("\n\n*******************"
+        "***********************");
+    printf("\n\nwelcome\n");
+    sleep(1);
+    clear();
+}
+
+int main(int args, char **argv)
+{
+    init_shell();
+
+    return 0;
+}
