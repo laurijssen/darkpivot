ad720f6acc38431c9de37e47a96e2bc05230af0f
laurijssen <49683709+laurijssen@users.noreply.github.com>
2023-02-16 11:46:04 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ void init_shell()
     clear();
     printf("\n\n******************"
         "************************");
-    printf("\n\I love nGummibears!");
+    printf("\n\nI love nGummibears!");
     printf("\n\n*******************"
         "***********************");
     printf("\n\nwelcome\n");
